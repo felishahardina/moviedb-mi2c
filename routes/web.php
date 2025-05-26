2c22b93bd4fd2b51dbc669aa88ff7cb777d064c7
felishahardina <felishahardina06@gmail.com>
2025-05-26 16:22:43 +0700

    form data movie

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MovieController::class, 'homepage']);
 
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+
+// Route::get('/detail-movie/{id}/{slug}', [MovieController::class, 'detail-movie']);
+
+Route::get('/create-movie', [MovieController::class, 'create']) ->name('movies.create');
+
+Route::get('/create-movies', [MovieController::class, 'create'])->name('movies.create');
+
+Route::post('/create-movies', [MovieController::class, 'store'])->name('movies.store');
