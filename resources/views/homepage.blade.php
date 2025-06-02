1b47fa89ec3e219759c5d20f65b3618526c1cfe6
felishahardina <felishahardina06@gmail.com>
2025-06-02 16:38:02 +0700

    login & logout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.template')
 
 @section('content')
+
 <h1> Latest Movie </h1>
 <div class="row">
     @foreach ($movies as $movie)
@@ -10,7 +11,7 @@
     <div class="col-md-4">
       <img src="{{ asset('covers/' . $movie->cover_image) }}" class="img-fluid rounded-start" alt="{{ $movie->title }}">
     </div>
-    <div class="col-md-8">
+    <div class="col-md-6">
       <div class="card-body">
         <h5 class="card-title"> {{$movie->title }}</h5>
         <p class="card-text"> {{ Str::words($movie->synopsis,20,'...')}}</p>
